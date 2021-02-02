2cb66b15b828522c1196af3c7a9ff9376feb9948
iPiotr <ip.starzec@gmail.com>
2021-02-02 23:31:52 +0100

    Add currency taken from the client settings trough Intl

--- script.js
+++ script.js
@@ -131,6 +131,15 @@ const formatMovementDate = (date, locale) => {
 
   return new Intl.DateTimeFormat(locale).format(date);
 
+};
+
+const formatCur = (value, locale, currency) => {
+  
+    return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+
 }
 
 const displayMovements = function(acc, sort = false) {
@@ -147,11 +156,13 @@ const displayMovements = function(acc, sort = false) {
     
     const displayDate = formatMovementDate(date, acc.locale);
 
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
       <div class="movements__date">${displayDate}</div>
-      <div class="movements__value">${mov.toFixed(2)}€</div>
+      <div class="movements__value">${formattedMov}</div>
     </div>
     `;
 
@@ -165,8 +176,10 @@ const totalAccountMovements = acc => {
 }
 
 const calcDisplayBalance = acc => {
+  
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)} EUR`;
+  
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);;
 }
 
 const calcDisplaySummary = (acc) => {
@@ -176,13 +189,13 @@ const calcDisplaySummary = (acc) => {
   .filter(mov => mov > 0)
   .reduce((acc, mov) => acc + mov, 0);
 
-  labelSumIn.textContent = `${incomes.toFixed(2)} €`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
   .filter(mov => mov < 0)
   .reduce((acc, mov) => acc + mov, 0);
 
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)} €`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -192,7 +205,7 @@ const calcDisplaySummary = (acc) => {
     })
     .reduce((acc, int) => acc + int, 0);
 
-    labelSumInterest.textContent = `${interest.toFixed(2)} €`;
+    labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);;
 
 };
 
@@ -253,15 +266,6 @@ btnLogin.addEventListener('click', (e) => {
     // weekday: 'long',
   }
   labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now);
-
-  // const locale = navigator.language;
-  // console.log(locale);
-    // const day = `${now.getDate()}`.padStart(2, 0);
-    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    // const year = now.getFullYear();
-    // const hour = `${now.getHours()}`.padStart(2, 0);
-    // const min = `${now.getMinutes()}`.padStart(2, 0);
-    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
@@ -346,4 +350,3 @@ btnSort.addEventListener('click', (e) => {
 
   sorted = !sorted;
 });
-
